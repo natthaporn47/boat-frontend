84a2ea52cbe101923eb6d757bed3ee6661463541
natthaporn <naththaphrnh@gmail.com>
2026-09-11 15:15:55 +0700

    modify navbar
    ลักษณะเมาส์ที่ชี้ ขนาดความปุ่ม

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,55 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import '../components/Navbar.css';
-import { Icon } from '@iconify/react';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../components/Navbar.css";
+import { Icon } from "@iconify/react";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false) // ☰ ปิดอยู่
+  const [isOpen, setIsOpen] = useState(false); // ☰ ปิดอยู่
   const closeMenu = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
   return (
     <>
-      <button className="menu-button" 
-      onClick={() => setIsOpen(!isOpen)}>☰</button> 
-      <div className={`navbar ${isOpen ? 'open' : ''}`}> <br/><br/><br/>
-        <NavLink to="/" onClick={closeMenu}
-        className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item'}>
-          <Icon icon="garden:home-stroke-12" /> 
+    {!isOpen && ( /* ปุ่มเปิด-ปิดเมนู การทำงาน*/
+      <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
+        <Icon icon="akar-icons:three-line-horizontal" />
+      </button>
+    )}
+      <div className={`navbar ${isOpen ? "open" : ""}`}>
+        <button class="close-button " onClick={() => setIsOpen(!isOpen)}>
+          <Icon icon="bitcoin-icons:cross-outline" />
+        </button>
+        <br />
+        <br />
+        <br />
+        <NavLink
+          to="/"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? "menu-item active" : "menu-item"
+          }
+        >
+          <Icon icon="garden:home-stroke-12" className="menu-icon" />
           หน้าหลัก
         </NavLink>
-        <NavLink to="/mission" onClick={closeMenu}
-        className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item'}>
-          <Icon icon="gis:map-route" /> สร้างภารกิจ
+        <NavLink
+          to="/mission"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? "menu-item active" : "menu-item"
+          }
+        >
+          <Icon icon="gis:map-route" className="menu-icon" /> สร้างภารกิจ
         </NavLink>
-        <NavLink to="/history" onClick={closeMenu}
-        className={({ isActive }) => isActive ? 'menu-item active' : 'menu-item'}>
-          <Icon icon="akar-icons:history" /> ประวัติการทำงาน
+        <NavLink
+          to="/history"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? "menu-item active" : "menu-item"
+          }
+        >
+          <Icon icon="akar-icons:history" className="menu-icon" />{" "}
+          ประวัติการทำงาน
         </NavLink>
       </div>
     </>
